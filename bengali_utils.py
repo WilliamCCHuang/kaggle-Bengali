471e05b3c5fe98dc05d2a477af24b15db670fd21
williamhuang <hang6318179@gmail.com>
2020-02-21 16:03:56 +0800

    [ADD] functions

--- bengali_utils.py
+++ bengali_utils.py
@@ -1,6 +1,14 @@
+import os
+import cv2
 import numpy as np
 import pandas as pd
-from sklearn.metrics import recall_score
+
+
+def create_dirs(file_path):
+    dir_path = os.path.dirname(file_path)
+
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
 
 
 def load_images(mode):
@@ -17,33 +25,39 @@ def load_images(mode):
     return df
 
 
-def multi_task_macro_recall(true_graphemes: np.array, pred_graphemes: np.array,
-                            true_vowels: np.array, pred_vowels: np.array,
-                            true_consonants: np.array, pred_consonants: np.array,
-                            n_grapheme=168, n_vowel=11, n_consonant=7):
-    
-    # pred_label_graphemes = torch.argmax(pred_graphemes, dim=1).cpu().numpy()
-    # true_label_graphemes = true_graphemes.cpu().numpy()
-    # pred_label_vowels = torch.argmax(pred_vowels, dim=1).cpu().numpy()
-    # true_label_vowels = true_vowels.cpu().numpy()
-    # pred_label_consonants = torch.argmax(pred_consonants, dim=1).cpu().numpy()
-    # true_label_consonants = true_consonants.cpu().numpy()
+def crop_char_image(image, threshold=5./255.):
+    assert image.ndim == 2
+    is_black = image > threshold
 
-    assert pred_graphemes.ndim == 1
-    assert true_graphemes.ndim == 1
-    assert pred_vowels.ndim == 1
-    assert true_vowels.ndim == 1
-    assert pred_consonants.ndim == 1
-    assert true_consonants.ndim == 1
+    is_black_vertical = np.sum(is_black, axis=0) > 0
+    is_black_horizontal = np.sum(is_black, axis=1) > 0
+    left = np.argmax(is_black_horizontal)
+    right = np.argmax(is_black_horizontal[::-1])
+    top = np.argmax(is_black_vertical)
+    bottom = np.argmax(is_black_vertical[::-1])
+    height, width = image.shape
+    cropped_image = image[left:height - right, top:width - bottom]
+    return cropped_image
 
-    grapheme_recall = recall_score(true_graphemes, pred_graphemes, labels=np.arange(n_grapheme), average='macro')
-    vowel_recall = recall_score(true_vowels, pred_vowels, labels=np.arange(n_vowel), average='macro')
-    consonant_recall = recall_score(true_consonants, pred_consonants, labels=np.arange(n_consonant), average='macro')
-    recalls = [grapheme_recall, vowel_recall, consonant_recall]
-    final_score = np.average(recalls, weights=[2, 1, 1])
 
-    return final_score, recalls
+def resize(image, size=128):
+    if isinstance(size, int):
+        size = (size, size)
+
+    return cv2.resize(image, size)
 
 
 def submit():
     pass # TODO:
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+
+    df_images = load_images(mode='test')
+    img = df_images.iloc[0][1:].values.reshape(137, 236).astype(np.uint8) / 255.0
+    img = 1 - img
+    img = crop_char_image(img, threshold=20.0/255.0)
+    img = resize(img, size=128)
+    plt.imshow(img)
+    plt.show()

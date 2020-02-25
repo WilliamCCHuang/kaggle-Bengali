1af7fbe35f0eb57ec65fb56547cd33dc539aae33
williamhuang <hang6318179@gmail.com>
2020-02-25 15:12:43 +0800

    [ADD] FocalLoss

--- pytorch_utils/losses.py
+++ pytorch_utils/losses.py
@@ -10,9 +10,12 @@ from torch.nn.modules.loss import _Loss, _WeightedLoss, CrossEntropyLoss
 
 __all__ = [
     'TopkCrossEntropyLoss',
+    'LabelSmoothingCrossEntropyLoss',
+    'FocalLoss',
     'MultiTaskLoss',
-    'MultiTaskCrossEntropyLoss'
-] # TODO: add FocalLoss after FocalLoss is done
+    'MultiTaskCrossEntropyLoss',
+    'MultiTaskLabelSmoothingCrossEntropyLoss'
+]
 
 
 class TopkCrossEntropyLoss(_WeightedLoss):
@@ -71,39 +74,64 @@ class LabelSmoothingCrossEntropyLoss(nn.Module):
 
 class FocalLoss(nn.Module):
     # TODO: check whether it is correct
-    def __init__(self, gamma=0, alpha=None, size_average=True):
-        super(FocalLoss, self).__init__()
-        self.gamma = gamma
+    """
+    Implemented by Albert
+    
+    Arguments:
+        nn {[type]} -- [description]
+    
+    Returns:
+        [type] -- [description]
+    """
+
+    def __init__(self, alpha=1, gamma=2, reduction='mean'):
         self.alpha = alpha
-        if isinstance(alpha, (float, int)): self.alpha = torch.Tensor([alpha, 1-alpha])
-        if isinstance(alpha, list): self.alpha = torch.Tensor(alpha)
-        self.size_average = size_average
+        self.gamma = gamma
+        self.reduction = reduction
 
     def forward(self, input, target):
-        if input.dim() > 2:
-            input = input.view(input.size(0), input.size(1), -1) # (N, C, H, W) => (N, C, H*W)
-            input = input.transpose(1, 2) # (N, C, H*W) => (N, H*W, C)
-            input = input.continguous().view(-1, input.size(2)) # (N*H*W, C)
-        target = target.view(-1, 1) # (N, 1)
+        ce_loss = F.cross_entropy(input, target, reduction=self.reduction=)
+        pt = torch.exp(-ce_loss)
+        focal_loss = self.alpha * (1 - pt) ** self.gamma * ce_loss
 
-        logpt = F.log_softmax(input)
-        logpt = logpt.gather(1, target)
-        logpt = logpt.view(-1)
-        pt = Variable(logpt.data.exp())
+        return focal_loss
 
-        if self.alpha is not None:
 
-            if self.alpha.type() != input.data.type():
-                self.alpha = self.alpha.type_as(input.data)
-            at = self.alpha.gather(0, target.data.view(-1))
-            logpt = logpt * Variable(at)
+# class FocalLoss(nn.Module):
+#     # TODO: check whether it is correct
+#     def __init__(self, gamma=0, alpha=None, size_average=True):
+#         super(FocalLoss, self).__init__()
+#         self.gamma = gamma
+#         self.alpha = alpha
+#         if isinstance(alpha, (float, int)): self.alpha = torch.Tensor([alpha, 1-alpha])
+#         if isinstance(alpha, list): self.alpha = torch.Tensor(alpha)
+#         self.size_average = size_average
 
-        loss = -1 * (1-pt)**self.gamma * logpt
+#     def forward(self, input, target):
+#         if input.dim() > 2:
+#             input = input.view(input.size(0), input.size(1), -1) # (N, C, H, W) => (N, C, H*W)
+#             input = input.transpose(1, 2) # (N, C, H*W) => (N, H*W, C)
+#             input = input.continguous().view(-1, input.size(2)) # (N*H*W, C)
+#         target = target.view(-1, 1) # (N, 1)
 
-        if self.size_average:
-            return loss.mean()
-        else:
-            return loss.sum()
+#         logpt = F.log_softmax(input)
+#         logpt = logpt.gather(1, target)
+#         logpt = logpt.view(-1)
+#         pt = Variable(logpt.data.exp())
+
+#         if self.alpha is not None:
+
+#             if self.alpha.type() != input.data.type():
+#                 self.alpha = self.alpha.type_as(input.data)
+#             at = self.alpha.gather(0, target.data.view(-1))
+#             logpt = logpt * Variable(at)
+
+#         loss = -1 * (1-pt)**self.gamma * logpt
+
+#         if self.size_average:
+#             return loss.mean()
+#         else:
+#             return loss.sum()
 
 
 class MultiTaskLoss(nn.Module):
@@ -212,4 +240,3 @@ if __name__ == "__main__":
     total_loss, losses = multi_task_criterion(inputs, targets)
     print(total_loss.item())
     print([loss.item() for loss in losses])
-    

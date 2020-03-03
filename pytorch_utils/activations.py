8d260b375a5751e8e1ef9eb4175fc7bda7506367
Rico Suave <49012231+RicoSuaveGuapo@users.noreply.github.com>
2020-03-03 14:25:40 +0800

    [ADD] Mish

---
+++ pytorch_utils/activations.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Mish(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        # inlining this saves 1 second per epoch (V100 GPU) vs having a temp x and then returning x(!)
+        return x * (torch.tanh(F.softplus(x)))

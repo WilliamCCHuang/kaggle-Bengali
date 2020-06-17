bb97477831a43bd11fa3e3bdde22aa1c1dbe0c00
RicoSuave <49012231+RicoSuaveGuapo@users.noreply.github.com>
2020-06-17 10:27:42 +0800

    Update FocalLoss
    add 'super().__init__()'  in  __init__

--- pytorch_utils/losses.py
+++ pytorch_utils/losses.py
@@ -85,6 +85,7 @@ class FocalLoss(nn.Module):
     """
 
     def __init__(self, alpha=1, gamma=2, reduction='mean'):
+        super().__init__()
         self.alpha = alpha
         self.gamma = gamma
         self.reduction = reduction
